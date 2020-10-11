01add0169a24bd6b1d9b32d48d9c4750984d4484
DESKTOP-NARAEL <adrianngaspar@gmail.com>
2020-10-11 15:12:38 +0200

    added icon lib, finished configurator core functions

--- src/Pages/Configuration.jsx
+++ src/Pages/Configuration.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import axios from 'axios'
 import Header from '../Components/Header'
 
+import { faArrowLeft, faArrowRight, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default class Configuration extends React.Component {
 
     productTypes = [
@@ -25,8 +28,38 @@ export default class Configuration extends React.Component {
             phone: "",
             email: "",
             totalPrice: 0,
-            browsing: "cpu"
+            browsing: this.productTypes[0],
+            configuratorStep: 0,
+            configuratorProgress: {
+                0: "Základnú Dosku",
+                1: "Procesor",
+                2: "Grafickú kartu",
+                3: "RAM-ky",
+                4: "Zdroj",
+                5: "SSD-čko",
+                6: "HDD-čko",
+                7: "Skrinku",
+                8: "Chladenie",
+            }
         }
+    }
+
+    handleProgress = (operation) => {
+        let step = this.state.configuratorStep
+        if (operation === "next") {
+            this.flushData()
+            step += 1
+            this.setState({ configuratorStep: step })
+        } else if (operation === "back") {
+            this.flushData()
+            step -= 1
+            this.setState({ configuratorStep: step })
+        }
+    }
+
+    flushData = () => {
+        const data = []
+        this.setState({ fetchedData: data })
     }
 
     fetchData = async (productType) => {
@@ -36,12 +69,8 @@ export default class Configuration extends React.Component {
         this.setState({ fetchedData: data })
     }
 
-    handleBrowsing = productType => {
-        this.setState({ browsing: productType })
-    }
-
     handleSearch = event => {
-        this.setState({ search: event.target.value }, () => this.fetchData(this.state.browsing))
+        this.setState({ search: event.target.value }, () => this.fetchData(this.productTypes[this.state.configuratorStep]))
     }
 
     handleEmail = event => {
@@ -92,11 +121,6 @@ export default class Configuration extends React.Component {
         )
     }
 
-    goBack = () => {
-        this.setState({ browsing: "" })
-        this.setState({ fetchedData: [] })
-    }
-
     showProducts = () => {
         return (
             <div 
@@ -114,8 +138,9 @@ export default class Configuration extends React.Component {
                     lg:mx-auto
                     xl:mx-auto
                     my-10">
-                {this.state.fetchedData.map(item => 
+                {this.state.fetchedData.map((item, key) => 
                     <div 
+                        key={key}
                         className="
                             font-mulish
                             grid 
@@ -127,6 +152,7 @@ export default class Configuration extends React.Component {
                         <h3>{item.model.toUpperCase()}</h3>
                         <div 
                             id="price-button"
+                            onClick={() => this.handleCart("add", item)}
                             className="
                                 transition duration-300 ease-in-out
                                 p-2 
@@ -141,6 +167,52 @@ export default class Configuration extends React.Component {
                         </div>
                     </div>
                 )}
+            </div>
+        )
+    }
+
+    nextAndBackButtons = () => {
+        return (
+            <div 
+                id="naspat-dalej"
+                className="
+                    flex 
+                    flex-row 
+                    justify-center 
+                    font-mulish 
+                    my-10">
+                <div 
+                    onClick={() => this.handleProgress("back")}
+                    className="
+                        flex
+                        flex-row
+                        transition duration-300 ease-in-out 
+                        mx-5 
+                        p-3 
+                        border-2 border-purple-600 
+                        text-black 
+                        hover:bg-purple-600 
+                        hover:text-white 
+                        cursor-pointer  
+                        rounded-full">
+                    <FontAwesomeIcon className="mx-2 self-center" icon={faArrowLeft}/>
+                    BACK
+                </div>
+                <div 
+                    onClick={() => this.handleProgress("next")}
+                    className="
+                        transition duration-300 ease-in-out 
+                        mx-5 
+                        p-3 
+                        border-2 border-purple-600 
+                        text-black 
+                        hover:bg-purple-600 
+                        hover:text-white 
+                        cursor-pointer 
+                        rounded-full">
+                    NEXT
+                    <FontAwesomeIcon className="mx-2 self-center" icon={faArrowRight}/>
+                </div>
             </div>
         )
     }
@@ -162,7 +234,7 @@ export default class Configuration extends React.Component {
                             lg:text-6xl 
                             md:text-3xl 
                             font-archivo">
-                        KONFIGURÁTOR
+                        ./KONFIGURÁTOR
                     </h1>
                 </div>
                 <div 
@@ -171,57 +243,26 @@ export default class Configuration extends React.Component {
                         flex 
                         flex-col 
                         self-center">
-                    <h1 className="text-center font-cairo">Vyhladaj CPU</h1>
-                    <input 
-                        className="shadow m-3 p-1"
-                        onChange={this.handleSearch}
-                        placeholder="napíš názov procesora">    
-                    </input>
+                    <h1 className="text-center font-cairo">Vyhľadávanie</h1>
+                    <div className="flex flex-row justify-center">
+                        <FontAwesomeIcon className="self-center" icon={faSearch}></FontAwesomeIcon>
+                        <input 
+                            className="shadow m-3 p-1"
+                            onChange={this.handleSearch}
+                            placeholder="hľadať">    
+                        </input>
+                    </div>
                     <h1 
                         className="
                             text-xl
                             md:text-2xl
                             lg:text-4xl
                             font-mulish">
-                        VYBER SI PROCESOR
+                        Vyber si {this.state.configuratorProgress[this.state.configuratorStep]}
                     </h1>
                 </div>
-
                 {this.showProducts()}
-                
-                <div 
-                    id="naspat-dalej"
-                    className="
-                        flex 
-                        flex-row 
-                        justify-center 
-                        font-mulish 
-                        my-10">
-                    <div className="
-                        transition duration-300 ease-in-out 
-                        mx-5 
-                        p-3 
-                        border-2 border-purple-600 
-                        text-black 
-                        hover:bg-purple-600 
-                        hover:text-white 
-                        cursor-pointer  
-                        rounded-full">
-                        BACK
-                    </div>
-                    <div className="
-                        transition duration-300 ease-in-out 
-                        mx-5 
-                        p-3 
-                        border-2 border-purple-600 
-                        text-black 
-                        hover:bg-purple-600 
-                        hover:text-white 
-                        cursor-pointer 
-                        rounded-full">
-                        NEXT
-                    </div>
-                </div>
+                {this.nextAndBackButtons()}
             </div>
         )
     }
