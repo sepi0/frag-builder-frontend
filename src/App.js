5c669cf9d4e090cab77adae740424c970f2089db
sepi0 <adrianngaspar@gmail.com>
2020-10-16 15:47:49 +0200

    Switched to Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+import {Route, Switch, BrowserRouter as Router, HashRouter} from 'react-router-dom'
 
 import Configuration from './Pages/Configuration'
 import Home from './Pages/Home'
 import Builds from './Pages/Builds'
 function App() {
   return (
-    <Router>
+    <HashRouter basename={'/'}>
       <Switch> 
         <Route exact path="/" component={Home}/>
         <Route exact path="/konfigurator" component={Configuration}/>
         <Route exact path="/zostavy" component={Builds}/>
       </Switch>
-    </Router>
+    </HashRouter>
   )
 }
 
