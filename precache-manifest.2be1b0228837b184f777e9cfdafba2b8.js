1786d2f180cce48e059439e3a1da030634da2d72
sepi0 <adrianngaspar@gmail.com>
2020-10-16 15:16:18 +0200

    Updates

---
+++ precache-manifest.2be1b0228837b184f777e9cfdafba2b8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135577919d8cd07402dbc29f5d9929d1",
+    "url": "/frag-builder-frontend/index.html"
+  },
+  {
+    "revision": "f0dcd28c33bd8dff2693",
+    "url": "/frag-builder-frontend/static/css/main.d71ba854.chunk.css"
+  },
+  {
+    "revision": "a1c5a6f5de719ce7fb81",
+    "url": "/frag-builder-frontend/static/js/2.f5e40f83.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/frag-builder-frontend/static/js/2.f5e40f83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0dcd28c33bd8dff2693",
+    "url": "/frag-builder-frontend/static/js/main.8e82e291.chunk.js"
+  },
+  {
+    "revision": "83293d06e36c0c38b1ee",
+    "url": "/frag-builder-frontend/static/js/runtime-main.e645dd79.js"
+  },
+  {
+    "revision": "8ac3f48a8e75419099ede3dee16b037a",
+    "url": "/frag-builder-frontend/static/media/aquaspikes.8ac3f48a.svg"
+  },
+  {
+    "revision": "f1a5bb2dfc84c22fd179c2cc7e292853",
+    "url": "/frag-builder-frontend/static/media/konfigurator.f1a5bb2d.svg"
+  },
+  {
+    "revision": "ac77a98bbe82ee5e002ccd9275edac47",
+    "url": "/frag-builder-frontend/static/media/purplespikes.ac77a98b.svg"
+  },
+  {
+    "revision": "36968e556800dfd439275f029133c972",
+    "url": "/frag-builder-frontend/static/media/zostavy.36968e55.svg"
+  }
+]);
